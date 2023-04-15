511071104bfeb6eeaff67f3ec9488e39f619d8ad
Brian Zhang <haoyuz@starsgroup.com>
2023-04-15 18:00:59 -0400

    initial commit

---
+++ go.mod
@@ -0,0 +1,10 @@
+module github.com/rainclear/oaknuts
+
+go 1.20
+
+require (
+	github.com/go-chi/chi v1.5.4
+	github.com/justinas/nosurf v1.1.1
+)
+
+require github.com/alexedwards/scs/v2 v2.5.1 // indirect
---
+++ pkg/render/render.go
@@ -0,0 +1,93 @@
+package render
+
+import (
+	"bytes"
+	"fmt"
+	"html/template"
+	"log"
+	"net/http"
+	"path/filepath"
+
+	"github.com/rainclear/oaknuts/pkg/config"
+	"github.com/rainclear/oaknuts/pkg/models"
+)
+
+var functions = template.FuncMap{}
+
+var app *config.AppConfig
+
+func NewTemplates(a *config.AppConfig) {
+	app = a
+}
+
+func AddDefaultData(td *models.TemplateData) *models.TemplateData {
+	return td
+}
+
+// RenderTemplate renders a template
+func RenderTemplate(w http.ResponseWriter, tmpl string, td *models.TemplateData) {
+	var tc map[string]*template.Template
+
+	if app.UseCache {
+		// get the template cache from the app config
+		tc = app.TemplateCache
+	} else {
+		var err error
+		tc, err = CreateTemplateCache()
+		if err != nil {
+			log.Fatal("Could not create temmplate cache")
+		}
+	}
+
+	t, ok := tc[tmpl]
+	if !ok {
+		log.Fatal("Could not get template from temmplate cache")
+	}
+
+	buf := new(bytes.Buffer)
+
+	td = AddDefaultData(td)
+
+	_ = t.Execute(buf, td)
+	_, err := buf.WriteTo(w)
+
+	if err != nil {
+		fmt.Println("Error writing template to browser", err)
+	}
+}
+
+func CreateTemplateCache() (map[string]*template.Template, error) {
+
+	tmplCache := map[string]*template.Template{}
+
+	pages, err := filepath.Glob("./templates/*.page.html")
+
+	if err != nil {
+		return tmplCache, err
+	}
+
+	for _, page := range pages {
+		name := filepath.Base(page)
+		ts, err := template.New(name).ParseFiles(page)
+		if err != nil {
+			return tmplCache, err
+		}
+
+		matches, err := filepath.Glob("./templates/*.layout.html")
+		if err != nil {
+			return tmplCache, err
+		}
+
+		if len(matches) > 0 {
+			ts, err = ts.ParseGlob("./templates/*.layout.html")
+			if err != nil {
+				return tmplCache, err
+			}
+
+		}
+
+		tmplCache[name] = ts
+	}
+
+	return tmplCache, nil
+}
